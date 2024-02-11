2812a6abf94adff3e8aa483cb4e3c8485f00319e
darryl-ty <98935440+darryl-ty@users.noreply.github.com>
2024-02-11 16:07:07 -0600

    Setup default member initialization for Courses class.

--- src/courses.h
+++ src/courses.h
@@ -12,11 +12,16 @@ class Courses {
 
 public:
     Courses(std::string_view semesterOffered, std::string_view department, std::string_view classNum, std::string_view sectionNum,
-            std::string_view className, std::string_view enrollLimit): m_semesterOffered(semesterOffered), m_department(department),
-            m_classNum(classNum), m_sectionNum(sectionNum), m_className(className), m_enrollLimit(enrollLimit)
-    {
+            std::string_view className, std::string_view enrollLimit, std::vector<std::string_view>& days,
+            std::vector<std::string_view>& time, std::vector<std::string_view>& building,
+            std::vector<std::string_view>& room, std::vector<std::string_view>& teachers):
+            m_semesterOffered(semesterOffered), m_department(department),
+            m_classNum(classNum), m_sectionNum(sectionNum), m_className(className),
+            m_enrollLimit(enrollLimit), m_days(days), m_time(time), m_building(building), m_room(room), m_teachers(teachers){
 
     }
+
+
 
 };
 
