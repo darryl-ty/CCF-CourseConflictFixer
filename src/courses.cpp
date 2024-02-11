d4ce745e3af60173ff713df9792c9861b54e5d0e
darryl-ty <98935440+darryl-ty@users.noreply.github.com>
2024-02-11 17:23:22 -0600

    Separate declaration and implementation of courses class. Added getters & setters.

--- src/courses.cpp
+++ src/courses.cpp
@@ -1,2 +1,101 @@
 
+//#include <string_view> TODO - Refactor all strings later to be string_views, we only need to view and compare strings, not modify.
+#include <string>
 #include "courses.h"
+
+Courses::Courses(std::string semesterOffered, std::string department, std::string classNum,
+                 std::string sectionNum, std::string className, std::string enrollLimit,
+                 std::vector<std::string> &days, std::vector<std::string> &time, std::vector<std::string> &building,
+                 std::vector<std::string> &room, std::vector<std::string> &teachers) : m_semesterOffered(semesterOffered), m_department(department),
+                 m_classNum(classNum), m_sectionNum(sectionNum), m_className(className),
+                 m_enrollLimit(enrollLimit), m_days(days), m_time(time), m_building(building), m_room(room), m_teachers(teachers){};
+
+Courses::Courses(){};
+
+void Courses::setSemesterOffered(const std::string& semester) {
+    m_semesterOffered = semester;
+}
+
+void Courses::setDepartment(const std::string& department) {
+    m_department = department;
+}
+
+void Courses::setMClassNum(const std::string& mClassNum) {
+    m_classNum = mClassNum;
+}
+
+void Courses::setMSectionNum(const std::string& mSectionNum) {
+    m_sectionNum = mSectionNum;
+}
+
+void Courses::setMClassName(const std::string& mClassName) {
+    m_className = mClassName;
+}
+
+void Courses::setMEnrollLimit(const std::string& mEnrollLimit) {
+    m_enrollLimit = mEnrollLimit;
+}
+
+void Courses::setMDays(const std::vector<std::string>& mDays) {
+    m_days = mDays;
+}
+
+void Courses::setMTime(const std::vector<std::string>& mTime) {
+    m_time = mTime;
+}
+
+void Courses::setMBuilding(const std::vector<std::string>& mBuilding) {
+    m_building = mBuilding;
+}
+
+void Courses::setMRoom(const std::vector<std::string>& mRoom) {
+    m_room = mRoom;
+}
+
+void Courses::setMTeachers(const std::vector<std::string>& mTeachers) {
+    m_teachers = mTeachers;
+}
+
+const std::string &Courses::getMSemesterOffered() const {
+    return m_semesterOffered;
+}
+
+const std::string &Courses::getMDepartment() const {
+    return m_department;
+}
+
+const std::string &Courses::getMClassNum() const {
+    return m_classNum;
+}
+
+const std::string &Courses::getMSectionNum() const {
+    return m_sectionNum;
+}
+
+const std::string &Courses::getMClassName() const {
+    return m_className;
+}
+
+const std::string &Courses::getMEnrollLimit() const {
+    return m_enrollLimit;
+}
+
+const std::vector<std::string> &Courses::getMDays() const {
+    return m_days;
+}
+
+const std::vector<std::string> &Courses::getMTime() const {
+    return m_time;
+}
+
+const std::vector<std::string> &Courses::getMBuilding() const {
+    return m_building;
+}
+
+const std::vector<std::string> &Courses::getMRoom() const {
+    return m_room;
+}
+
+const std::vector<std::string> &Courses::getMTeachers() const {
+    return m_teachers;
+}
--- src/courses.h
+++ src/courses.h
@@ -7,20 +7,59 @@
 
 
 class Courses {
-    std::string_view m_semesterOffered, m_department, m_classNum, m_sectionNum, m_className, m_enrollLimit;
-    std::vector<std::string_view> m_days, m_time, m_building, m_room, m_teachers;
+    std::string m_semesterOffered, m_department, m_classNum, m_sectionNum, m_className, m_enrollLimit;
+    std::vector<std::string> m_days, m_time, m_building, m_room, m_teachers;
 
 public:
-    Courses(std::string_view semesterOffered, std::string_view department, std::string_view classNum, std::string_view sectionNum,
-            std::string_view className, std::string_view enrollLimit, std::vector<std::string_view>& days,
-            std::vector<std::string_view>& time, std::vector<std::string_view>& building,
-            std::vector<std::string_view>& room, std::vector<std::string_view>& teachers):
-            m_semesterOffered(semesterOffered), m_department(department),
-            m_classNum(classNum), m_sectionNum(sectionNum), m_className(className),
-            m_enrollLimit(enrollLimit), m_days(days), m_time(time), m_building(building), m_room(room), m_teachers(teachers){
+    Courses(std::string semesterOffered, std::string department, std::string classNum, std::string sectionNum,
+            std::string className, std::string enrollLimit, std::vector<std::string>& days,
+            std::vector<std::string>& time, std::vector<std::string>& building,
+            std::vector<std::string>& room, std::vector<std::string>& teachers);
+    Courses();
 
-    }
+    void setSemesterOffered(const std::string& semester);
 
+    void setDepartment(const std::string& department);
+
+    void setMClassNum(const std::string &mClassNum);
+
+    void setMSectionNum(const std::string &mSectionNum);
+
+    void setMClassName(const std::string &mClassName);
+
+    void setMEnrollLimit(const std::string &mEnrollLimit);
+
+    void setMDays(const std::vector<std::string> &mDays);
+
+    void setMTime(const std::vector<std::string> &mTime);
+
+    void setMBuilding(const std::vector<std::string> &mBuilding);
+
+    void setMRoom(const std::vector<std::string> &mRoom);
+
+    void setMTeachers(const std::vector<std::string> &mTeachers);
+
+    const std::string &getMSemesterOffered() const;
+
+    const std::string &getMDepartment() const;
+
+    const std::string &getMClassNum() const;
+
+    const std::string &getMSectionNum() const;
+
+    const std::string &getMClassName() const;
+
+    const std::string &getMEnrollLimit() const;
+
+    const std::vector<std::string> &getMDays() const;
+
+    const std::vector<std::string> &getMTime() const;
+
+    const std::vector<std::string> &getMBuilding() const;
+
+    const std::vector<std::string> &getMRoom() const;
+
+    const std::vector<std::string> &getMTeachers() const;
 
 
 };
