25fde13e36927650e3c6c5c1bcd7e584e2b949ff
darryl-ty <darrylterrell921@gmail.com>
2024-02-25 16:11:34 -0600

    Setup simple test to check the if semester offered is the same.

--- CourseConflicFixerWithTests/CourseTest.cpp
+++ CourseConflicFixerWithTests/CourseTest.cpp
@@ -1,5 +1,14 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "../../CourseConflictFixer/src/Course.h"
 #include "doctest.h"
 
-TEST_CASE(){
-    
+Course testCourse = {"202408", "CPSC","1005","1","Intro to PCs and Software Appls",
+                     std::vector<std::string>{"20"},std::vector<std::string>{"TR"},
+                     std::vector<std::string>{"9:25-10:40 am"},std::vector<std::string>{"37"},
+                     std::vector<std::string>{"570"},std::vector<std::string>{"Kun Zhang"}};
+
+TEST_CASE("Test different variables of Course objects."){
+
+    CHECK((testCourse.getSemesterOffered()) == ("202408"));
 }
