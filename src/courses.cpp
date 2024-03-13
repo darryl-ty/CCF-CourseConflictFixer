fe37902e4ecf1c2e43a810250925e85928b706e0
darryl-ty <darrylterrell921@gmail.com>
2024-03-12 22:07:10 -0500

    Add courses class to hold a vector of course objects.

---
+++ src/courses.cpp
@@ -0,0 +1,2 @@
+
+#include "Courses.h"
---
+++ src/courses.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef COURSES_H
+#define COURSES_H
+#include "Course.h"
+
+class Courses {
+
+    std::vector<Course> course;
+
+
+};
+
+
+#endif
