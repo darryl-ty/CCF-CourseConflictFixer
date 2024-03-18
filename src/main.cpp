74d6832a93c96486bf4401c99eb244e49033e64b
darryl-ty <darrylterrell921@gmail.com>
2024-03-17 20:25:22 -0500

    Start refactoring main.cpp for new Courses object.

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,20 @@
 #include <sstream>
 #include <cassert>
 
+#include "courses.h"
 #include "course.h"
 
-std::vector<Course> readCoursesCSV(const std::string&);
+Courses readCoursesCSV(const std::string&);
 
-void compareCourseEntries(const std::vector<Course> &courses);
-void testCourses(const std::vector<Course> &courses);
+void compareCourseEntries(const Courses &courses);
+void testCourses(const Courses &courses);
 
 int main() {
     const std::string coursesFile = "courses.csv";
 
     std::cout << "Welcome to the CCF - Course Conflict Fixer!" << std::endl;
-    std::vector<Course> *coursePtr;
-    std::vector<Course> courses = readCoursesCSV(coursesFile);
+    Courses *coursePtr;
+    Courses courses = readCoursesCSV(coursesFile);
     coursePtr = &courses;
 
     compareCourseEntries(*coursePtr);
@@ -29,11 +30,11 @@ int main() {
     return 0;
 }
 
-void compareCourseEntries(const std::vector<Course> &courses) {
+void compareCourseEntries(const Courses &courses) {
 
-    for (size_t i = 0; i < courses.size(); i++) {
-        for (size_t j = i+1; j < courses.size(); j++) {
-            courses[i].compareCourses(courses[j]);
+    for (size_t i = 0; i < courses.getCourses().size(); i++) {
+        for (size_t j = i+1; j < courses.getCourses().size(); j++) {
+            courses.getCourses()[i].compareCourses(courses.getCourses()[j]);
             std::cout << "Course " << i << " and Course " << j << std::endl;
         }
     }
@@ -42,7 +43,7 @@ void compareCourseEntries(const std::vector<Course> &courses) {
 std::vector<Course> readCoursesCSV(const std::string& input) {
     std::ifstream file(input);
     std::string line;
-    std::vector<Course> courses;
+    Courses courses;
 
     std::getline(file,line); //skips header
     while(std::getline(file, line)){
@@ -112,7 +113,7 @@ std::vector<Course> readCoursesCSV(const std::string& input) {
             }
         }
 
-        courses.emplace_back(semester,dept,classNum,section,className,enrollLimit,days,time,building,room,teachers);
+//        courses.emplace_back(semester,dept,classNum,section,className,enrollLimit,days,time,building,room,teachers);
 
     }
 
@@ -121,42 +122,42 @@ std::vector<Course> readCoursesCSV(const std::string& input) {
 }
 
 
-void testCourses(const std::vector<Course> &courses) {
+void testCourses(const Courses &courses) {
     std::ostringstream output;
     std::streambuf *outputBuffer = std::cout.rdbuf(); // Create ptr for cout buffer we can reference later on.
 
     std::cout.rdbuf(output.rdbuf()); // Change cout to a "local" output buffer that we can test.
 
-    assert(!courses.empty());
+    assert(!courses.getCourses().empty());
 
-    assert(!courses[0].getSemesterOffered().empty());
-    assert("202301" != courses[0].getSemesterOffered());
-    assert("202408" == courses[0].getSemesterOffered());
-    assert("PSYCH" != courses[0].getDepartment());
-    assert("CPSC" == courses[0].getDepartment());
+    assert(!courses.getCourses()[0].getSemesterOffered().empty());
+    assert("202301" != courses.getCourses()[0].getSemesterOffered());
+    assert("202408" == courses.getCourses()[0].getSemesterOffered());
+    assert("PSYCH" != courses.getCourses()[0].getDepartment());
+    assert("CPSC" == courses.getCourses()[0].getDepartment());
 
-    assert(!courses[1].getSemesterOffered().empty());
-    assert("202408" != courses[1].getSemesterOffered());
-    assert("202501" == courses[1].getSemesterOffered());
-    assert("ENGR" != courses[0].getDepartment());
-    assert("CPSC" == courses[0].getDepartment());
+    assert(!courses.getCourses()[1].getSemesterOffered().empty());
+    assert("202408" != courses.getCourses()[1].getSemesterOffered());
+    assert("202501" == courses.getCourses()[1].getSemesterOffered());
+    assert("ENGR" != courses.getCourses()[0].getDepartment());
+    assert("CPSC" == courses.getCourses()[0].getDepartment());
 
 
-    courses[0].compareCourses(courses[1]);
+    courses.getCourses()[0].compareCourses(courses.getCourses()[1]);
     std::cout.rdbuf(outputBuffer); // Change cout back to its original buffer
     assert(output.str() == "No detected course conflicts. ");
     output.str(""); // Set output to be empty.
     output.clear(); // Clear state of output for next assertion.
     std::cout.rdbuf(output.rdbuf()); // Change cout back to a "local" output buffer that we can test for next assertion.
 
-    courses[0].compareCourses(courses[4]);
+    courses.getCourses()[0].compareCourses(courses.getCourses()[4]);
     std::cout.rdbuf(outputBuffer);
     assert(output.str() == "Non-critical course conflict detected. ");
     output.str("");
     output.clear();
     std::cout.rdbuf(output.rdbuf());
 
-    courses[4].compareCourses(courses[5]);
+    courses.getCourses()[4].compareCourses(courses.getCourses()[5]);
     std::cout.rdbuf(outputBuffer);
     assert(output.str() == "Time-Room conflict detected! ");
     output.str("");
