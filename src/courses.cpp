0d4a5d632c711ed9370d79d65217518e8aeb8b61
darryl-ty <darrylterrell921@gmail.com>
2024-03-24 15:14:30 -0500

    Mark compareCourseObjects method const.

--- src/courses.cpp
+++ src/courses.cpp
@@ -16,6 +16,12 @@ std::vector<Course> Courses::getCourses() const {
     return m_courses;
 }
 
-void Courses::compareCourseObjects() {
+void Courses::compareCourseObjects() const {
 
+    for (size_t i = 0; i < this->getCourses().size(); i++) {
+        for (size_t j = i+1; j < this->getCourses().size(); j++) {
+            this->getCourses()[i].compareCourses(this->getCourses()[j]);
+            std::cout << "Course " << i << " and Course " << j << std::endl;
+        }
+    }
 }
